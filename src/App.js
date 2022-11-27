a4289261cdd3ccdd77a57b2bff22a8befb39720a
Rukyy <rukemeniemogha38@gmail.com>
2022-11-27 01:47:31 +0100

    Install react-router-dom for routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import { useState } from 'react';
+import { BrowserRouter as Router,Route,Switch,Routes } from 'react-router-dom';
 import './App.css';
+import Cart from './components/cart/cart';
 import Home from './components/home/home';
 import NavBar from './components/navbar/navbar';
 
 function App() {
 
   const [state, setstate]=useState(0)
-  
   return (
-    <div className="App">
-      <NavBar state={state} />
-      <Home setstate={setstate}/>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar state={state}/>
+        <div className="content">
+          <Switch>
+            <Route exact path='/'>
+              <Home setstate={setstate}/>
+            </Route>
+            <Route exact path='/'>
+              <Cart prop={state}/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
